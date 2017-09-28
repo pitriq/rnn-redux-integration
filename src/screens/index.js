344efda94d9f4a2db681bf7831a90b7dffd5faba
Unknown <franpitri@gmail.com>
2017-09-28 16:21:59 -0300

    add Redux's provider & store to screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -6,9 +6,9 @@ import FinancesScreen from '../containers/FinancesScreen'
 import DiscoverScreen from '../containers/DiscoverScreen'
 
 // register all screens of the app (including internal ones)
-export function registerScreens() {
-    Navigation.registerComponent('LoginScreen', () => LoginScreen)
-    Navigation.registerComponent('FinancesScreen', () => FinancesScreen)
-    Navigation.registerComponent('NewsScreen', () => NewsScreen)
-    Navigation.registerComponent('DiscoverScreen', () => DiscoverScreen)
+export function registerScreens(store, Provider) {
+    Navigation.registerComponent('LoginScreen', () => LoginScreen, store, Provider)
+    Navigation.registerComponent('FinancesScreen', () => FinancesScreen, store, Provider)
+    Navigation.registerComponent('NewsScreen', () => NewsScreen, store, Provider)
+    Navigation.registerComponent('DiscoverScreen', () => DiscoverScreen, store, Provider)
 }
